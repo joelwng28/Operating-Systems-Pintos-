6d7e8000067260347ef717c095b832fb1fb4e815
Rahul Sharma <rahuls0720@gmail.com>
2017-10-07 13:07:06 -0500

    implemented basic structure for all syscalls; refactored stack access code

--- userprog/syscall.c
+++ userprog/syscall.c
@@ -9,6 +9,14 @@
 
 typedef int pid_t;
 
+//for peek at parts of stack
+#define syscallSize 1
+#define intSize 1 
+#define ptrSize 1
+
+#define STDIN_FILENO 0
+#define STDOUT_FILENO 1
+
 static void syscall_handler (struct intr_frame *);
 
 //from user/lib/syscall.h
@@ -57,13 +65,9 @@ that will be returned. Conventionally, a status of 0 indicates success
 and nonzero values indicate errors. */
 static void 
 exit(int status){ //todo: tell waiting parent, child exited
-  if(isAddressValid(&status)){
     struct thread* curr = thread_current();
-    //todo: not sure acutual termination message, so put dumby one
-    printf ("process %s is exit. Status: (%d)\n", curr->name, curr->status); 
-
+    printf("%s: exit(%d)\n", thread_name(), status);
     thread_exit();
-  }
 }
 
 /*Runs the executable whose name is given in cmd_line, passing any given arguments, 
@@ -74,18 +78,14 @@ until it knows whether the child process successfully loaded its executable.
 You must use appropriate synchronization to ensure this.*/
 static pid_t
 exec(const char *cmd_line){ //todo: add synchronization for parent/child
-  if(isAddressValid(&cmd_line)){
-   return process_execute(cmd_line);
- }
+  return process_execute(cmd_line);
 }
 
 /*Waits for a child process pid and retrieves the child's exit status.*/
 static int
 wait(pid_t pid){
   //todo: return status
-   if(isAddressValid(&pid)){
-    process_wait();
-  }
+  process_wait();
 }
 
 /*Creates a new file called file initially initial_size bytes in size.
@@ -93,11 +93,8 @@ wait(pid_t pid){
  opening the new file is a separate operation which would require a open system call.*/
 static bool 
 create (const char *file, unsigned initial_size){
-  //todo: synch
-  if(isAddressValid(&file) && isAddressValid(&initial_size)){
-    if(filesys_create(file, initial_size)){
+ if(filesys_create(file, initial_size)){ //todo: synch
       return true;
-    }
   }
   return false;
 }
@@ -107,10 +104,8 @@ A file may be removed regardless of whether it is open or closed,
 and removing an open file does not close it. See Removing an Open File, for details.*/
 static bool
 remove (const char *file){
-  if(isAddressValid(&file)){
-     if(filesys_remove(file)){
-      return true;
-     }
+   if(filesys_remove(file)){
+    return true;
    }
    return false;
 }
@@ -118,10 +113,60 @@ remove (const char *file){
 /*Opens the file called file. Returns a nonnegative integer handle 
 called a "file descriptor" (fd), or -1 if the file could not be opened.*/
 static int
-open (const char *file){
-  if(isAddressValid(&file)){
-    return filesys_open(file);
+open (const char *file){ //implement FD
+  return filesys_open(file);
+}
+
+/*Reads size bytes from the file open as fd into buffer.
+ Returns the number of bytes actually read (0 at end of file), 
+ or -1 if the file could not be read (due to a condition other than end of file).
+ Fd 0 reads from the keyboard using input_getc().*/
+static int
+read (int fd, void *buffer, unsigned length){
+  if(fd == STDIN_FILENO){
+      uint8_t* local_buffer = (uint8_t *) buffer;
+      for (int i = 0; i < length; i++){
+        local_buffer[i] = input_getc();
+      }
+      return size;
   }
+  //file_read()
+}
+
+/*Writes size bytes from buffer to the open file fd.
+  Returns the number of bytes actually written, 
+  which may be less than size if some bytes could not be written.*/
+static int
+write (int fd, const void *buffer, unsigned length){
+  if(fd == STDOUT_FILENO){
+    putbuf(buffer, length);
+    return length; //no EOF for console
+  }
+  //todo: implement FDT and write to it
+  //use file_write() to write to file
+}
+
+/*Changes the next byte to be read or written in open file fd to position,
+  expressed in bytes from the beginning of the file. 
+  (Thus, a position of 0 is the file's start.)*/
+static void
+seek (int fd, unsigned position){
+
+}
+ 
+ /*Returns the position of the next byte to be read or written in open file fd,
+  expressed in bytes from the beginning of the file.*/
+static unsigned 
+tell (int fd){
+  
+}
+
+/*Closes file descriptor fd.
+ Exiting or terminating a process implicitly closes all its open file descriptors,
+  as if by calling this function for each one.*/
+static void 
+close (int fd){
+
 }
 
 static void
@@ -136,28 +181,62 @@ syscall_handler (struct intr_frame *f UNUSED)
     exit(-1);
   }
 
+  void *arg1, *arg2, *arg3;
   switch (syscall_number){
       case SYS_HALT:
           halt();
           break;
       case SYS_EXIT:
-          f->error_code = *(esp + 4); //return to kernel?
-          exit(*(esp + 4));
+          arg1 = esp + syscallSize;
+          if(isAddressValid(arg1)){
+            f->error_code = *arg1; //return to kernel?
+            exit(*arg1);
+          }
           break;
       case SYS_EXEC:
-    	    f->eax = exec(*(esp + 4));
+          arg1 = esp + syscallSize;
+          if(isAddressValid(arg1)){ f->eax = exec(*arg1); }
     	    break;
       case SYS_WAIT:
-          wait(*(esp + 4));
+          arg1 = esp + syscallSize;
+          if(isAddressValid(arg1)){ wait(*arg1); }
           break;
       case SYS_CREATE:
-          f->eax = create(*(esp + 4), *(esp + 5));
+          arg1 = esp + syscallSize;
+          arg2 = esp + syscallSize + ptrSize;
+          if(isAddressValid(arg1) && isAddressValid(arg2)){ f->eax = create(*arg1, *arg2); }
           break;
       case SYS_REMOVE:
-          f->eax = remove(*(esp + 4));
+          arg1 = esp + syscallSize;
+          if(isAddressValid(arg1)){ f->eax = remove(*arg1); }
           break;
       case SYS_OPEN:
-          f->eax = open(*(esp + 4));
+          arg1 = esp + syscallSize;
+          if(isAddressValid(arg1)){ f->eax = open(*arg1); }
+          break;
+      case SYS_FILESIZE:
+          //filesize(*(esp + syscallSize));
+          break;
+      case SYS_READ: 
+          arg1 = esp + syscallSize;
+          arg2 = esp + syscallSize + intSize;
+          arg3 = esp + syscallSize + intSize + ptrSize;
+          if(isAddressValid){ read(*arg1, *arg2, *arg3); }
+          break;
+      case SYS_WRITE: 
+          arg1 = esp + syscallSize;
+          arg2 = esp + syscallSize + intSize;
+          arg3 = esp + syscallSize + intSize + ptrSize;
+          if(isAddressValid){ write(*arg1, *arg2, *arg3); }
+          break;
+      case SYS_SEEK:
+          //seek(*(esp + syscallSize), *(esp + syscallSize + intSize));
+          break;
+      case SYS_TELL:
+          //tell(*(esp + syscallSize));
+          break;
+      case SYS_CLOSE:
+          //close(*(esp + syscallSize));
           break;
       default:
           break;
