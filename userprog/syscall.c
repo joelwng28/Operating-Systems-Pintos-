1735d5bcd569e6973e7d4aac44afabfe67016d8f
Rahul Sharma <rahuls0720@gmail.com>
2017-10-07 00:51:11 -0500

    added main functionality for syscalls halt, exit, exec, wait, create, remove, open

--- userprog/syscall.c
+++ userprog/syscall.c
@@ -3,8 +3,28 @@
 #include <syscall-nr.h>
 #include "threads/interrupt.h"
 #include "threads/thread.h"
+#include "threads/vaddr.h"
+#include <stdbool.h>
+#include "filesys/filesys.h"
+
+typedef int pid_t;
 
 static void syscall_handler (struct intr_frame *);
+
+//from user/lib/syscall.h
+// void halt (void) NO_RETURN;
+// void exit (int status) NO_RETURN;
+// pid_t exec (const char *file);
+// int wait (pid_t);
+// bool create (const char *file, unsigned initial_size);
+// bool remove (const char *file);
+// int open (const char *file);
+// int filesize (int fd);
+// int read (int fd, void *buffer, unsigned length);
+// int write (int fd, const void *buffer, unsigned length);
+// void seek (int fd, unsigned position);
+// unsigned tell (int fd);
+// void close (int fd);
 
 void
 syscall_init (void)
@@ -12,300 +32,136 @@ syscall_init (void)
   intr_register_int (0x30, 3, INTR_ON, syscall_handler, "syscall");
 }
 
-bool
-create (const char *file_name, unsigned size)
-{
-  bool status;
-
-  if (!is_valid_ptr (file_name))
-    exit (-1);
-
-  lock_acquire (&fs_lock);
-  status = filesys_create(file_name, size);  
-  lock_release (&fs_lock);
-  return status;
+//helper function to ensure pointer is in user space, not null, and mapped
+static bool
+isAddressValid(void *pointer){
+  void *ptr = pagedir_get_page(thread_current()->pagedir, pointer);
+  if(!is_user_vaddr(pointer) || (pointer == 0) || !ptr){
+      exit(-1);
+      return false;
+  }
+  return true;
 }
 
-bool 
-remove (const char *file_name)
-{
-  bool status;
-  if (!is_valid_ptr (file_name))
-    exit (-1);
-
-  lock_acquire (&fs_lock);  
-  status = filesys_remove (file_name);
-  lock_release (&fs_lock);
-  return status;
+/* Terminates Pintos by calling shutdown_power_off() 
+(declared in threads/init.h). This should be seldom used, 
+because you lose some information about possible deadlock situations, etc.*/
+static void 
+halt(void){ 
+  shutdown_power_off();
 }
 
-int
-open (const char *file_name)
-{
-  struct file *f;
-  struct file_descriptor *fd;
-  int status = -1;
-  
-  if (!is_valid_ptr (file_name))
-    exit (-1);
+/*Terminates the current user program, returning status to the kernel. 
+If the process's parent waits for it (see below), this is the status 
+that will be returned. Conventionally, a status of 0 indicates success 
+and nonzero values indicate errors. */
+static void 
+exit(int status){ //todo: tell waiting parent, child exited
+  if(isAddressValid(&status)){
+    struct thread* curr = thread_current();
+    //todo: not sure acutual termination message, so put dumby one
+    printf ("process %s is exit. Status: (%d)\n", curr->name, curr->status); 
 
-  lock_acquire (&fs_lock); 
- 
-  f = filesys_open (file_name);
-  if (f != NULL)
-    {
-      fd = calloc (1, sizeof *fd);
-      fd->fd_num = allocate_fd ();
-      fd->owner = thread_current ()->tid;
-      fd->file_struct = f;
-      list_push_back (&open_files, &fd->elem);
-      status = fd->fd_num;
+    thread_exit();
+  }
+}
+
+/*Runs the executable whose name is given in cmd_line, passing any given arguments, 
+and returns the new process's program id (pid). Must return pid -1, 
+which otherwise should not be a valid pid, if the program cannot load or run for any reason. 
+Thus, the parent process cannot return from the exec
+until it knows whether the child process successfully loaded its executable.
+You must use appropriate synchronization to ensure this.*/
+static pid_t
+exec(const char *cmd_line){ //todo: add synchronization for parent/child
+  if(isAddressValid(&cmd_line)){
+   return process_execute(cmd_line);
+ }
+}
+
+/*Waits for a child process pid and retrieves the child's exit status.*/
+static int
+wait(pid_t pid){
+  //todo: return status
+   if(isAddressValid(&pid)){
+    process_wait();
+  }
+}
+
+/*Creates a new file called file initially initial_size bytes in size.
+ Returns true if successful, false otherwise. Creating a new file does not open it:
+ opening the new file is a separate operation which would require a open system call.*/
+static bool 
+create (const char *file, unsigned initial_size){
+  //todo: synch
+  if(isAddressValid(&file) && isAddressValid(&initial_size)){
+    if(filesys_create(file, initial_size)){
+      return true;
     }
-  lock_release (&fs_lock);
-  return status;
+  }
+  return false;
 }
 
-int
-filesize (int fd)
-{
-  struct file_descriptor *fd_struct;
-  int status = -1;
-  lock_acquire (&fs_lock); 
-  fd_struct = get_open_file (fd);
-  if (fd_struct != NULL)
-    status = file_length (fd_struct->file_struct);
-  lock_release (&fs_lock);
-  return status;
+/*Deletes the file called file. Returns true if successful, false otherwise.
+A file may be removed regardless of whether it is open or closed,
+and removing an open file does not close it. See Removing an Open File, for details.*/
+static bool
+remove (const char *file){
+  if(isAddressValid(&file)){
+     if(filesys_remove(file)){
+      return true;
+     }
+   }
+   return false;
 }
 
-int
-read (int fd, void *buffer, unsigned size)
-{
-  struct file_descriptor *fd_struct;
-  int status = 0;
-  struct thread *t = thread_current ();
-
-  unsigned buffer_size = size;
-  void * buffer_tmp = buffer;
-
-  /* check the user memory pointing by buffer are valid */
-  while (buffer_tmp != NULL)
-    {
-      if (!is_valid_uvaddr (buffer_tmp))
-	exit (-1);
-
-      if (pagedir_get_page (t->pagedir, buffer_tmp) == NULL)   
-	{ 
-	  struct suppl_pte *spte;
-	  spte = get_suppl_pte (&t->suppl_page_table, 
-				pg_round_down (buffer_tmp));
-	  if (spte != NULL && !spte->is_loaded)
-	    load_page (spte);
-          else if (spte == NULL && buffer_tmp >= (esp - 32))
-	    grow_stack (buffer_tmp);
-	  else
-	    exit (-1);
-	}
-      
-      /* Advance */
-      if (buffer_size == 0)
-	{
-	  /* terminate the checking loop */
-	  buffer_tmp = NULL;
-	}
-      else if (buffer_size > PGSIZE)
-	{
-	  buffer_tmp += PGSIZE;
-	  buffer_size -= PGSIZE;
-	}
-      else
-	{
-	  /* last loop */
-	  buffer_tmp = buffer + size - 1;
-	  buffer_size = 0;
-	}
-    }
-
-  lock_acquire (&fs_lock);   
-  if (fd == STDOUT_FILENO)
-      status = -1;
-  else if (fd == STDIN_FILENO)
-    {
-      uint8_t c;
-      unsigned counter = size;
-      uint8_t *buf = buffer;
-      while (counter > 1 && (c = input_getc()) != 0)
-        {
-          *buf = c;
-          buffer++;
-          counter--; 
-        }
-      *buf = 0;
-      status = size - counter;
-    }
-  else 
-    {
-      fd_struct = get_open_file (fd);
-      if (fd_struct != NULL)
-	status = file_read (fd_struct->file_struct, buffer, size);
-    }
-  lock_release (&fs_lock);
-  return status;
-}
-
-int
-write (int fd, const void *buffer, unsigned size)
-{
-  struct file_descriptor *fd_struct;  
-  int status = 0;
-
-  unsigned buffer_size = size;
-  void *buffer_tmp = buffer;
-
-  /* check the user memory pointing by buffer are valid */
-  while (buffer_tmp != NULL)
-    {
-      if (!is_valid_ptr (buffer_tmp))
-	exit (-1);
-      
-      /* Advance */ 
-      if (buffer_size > PGSIZE)
-	{
-	  buffer_tmp += PGSIZE;
-	  buffer_size -= PGSIZE;
-	}
-      else if (buffer_size == 0)
-	{
-	  /* terminate the checking loop */
-	  buffer_tmp = NULL;
-	}
-      else
-	{
-	  /* last loop */
-	  buffer_tmp = buffer + size - 1;
-	  buffer_size = 0;
-	}
-    }
-
-  lock_acquire (&fs_lock); 
-  if (fd == STDIN_FILENO)
-    {
-      status = -1;
-    }
-  else if (fd == STDOUT_FILENO)
-    {
-      putbuf (buffer, size);;
-      status = size;
-    }
-  else 
-    {
-      fd_struct = get_open_file (fd);
-      if (fd_struct != NULL)
-	status = file_write (fd_struct->file_struct, buffer, size);
-    }
-  lock_release (&fs_lock);
-
-  return status;
-}
-
-void 
-seek (int fd, unsigned position)
-{
-  struct file_descriptor *fd_struct;
-  lock_acquire (&fs_lock); 
-  fd_struct = get_open_file (fd);
-  if (fd_struct != NULL)
-    file_seek (fd_struct->file_struct, position);
-  lock_release (&fs_lock);
-  return ;
-}
-
-unsigned 
-tell (int fd)
-{
-  struct file_descriptor *fd_struct;
-  int status = 0;
-  lock_acquire (&fs_lock); 
-  fd_struct = get_open_file (fd);
-  if (fd_struct != NULL)
-    status = file_tell (fd_struct->file_struct);
-  lock_release (&fs_lock);
-  return status;
-}
-
-void 
-close (int fd)
-{
-  struct file_descriptor *fd_struct;
-  lock_acquire (&fs_lock); 
-  fd_struct = get_open_file (fd);
-  if (fd_struct != NULL && fd_struct->owner == thread_current ()->tid)
-    close_open_file (fd);
-  lock_release (&fs_lock);
-  return ; 
-}
-
-int*
-pop(int *ptr){
-	if(!is_user_vaddr(ptr)){
-		exit(-1);
-	}
-	return ptr + 1;
+/*Opens the file called file. Returns a nonnegative integer handle 
+called a "file descriptor" (fd), or -1 if the file could not be opened.*/
+static int
+open (const char *file){
+  if(isAddressValid(&file)){
+    return filesys_open(file);
+  }
 }
 
 static void
 syscall_handler (struct intr_frame *f UNUSED)
 {
-	int *esp = f->esp;
-	int syscall_number = *esp;
-	if(!is_user_vaddr(syscall_number)){
-		exit(-1);
-	}
-	switch (syscall_number)
-        {
-        case SYS_HALT:
-          	shutdown_power_off ();
-          	break;
-        case SYS_EXIT:
-          	exit (*pop(esp));
-          	break;
-         //process.c
-        case SYS_EXEC:
-          	f->eax = exec ((char *) *pop(esp));
-          	break;
-         //process.c
-        case SYS_WAIT:
-          	f->eax = process_wait(*pop(esp));
-          	break;
-        case SYS_CREATE:
-          	f->eax = create ((char *) *pop(esp), *pop(esp);
-          	break;
-        case SYS_REMOVE:
-          	f->eax = remove ((char *) *pop(esp));
-          	break;
-        case SYS_OPEN:
-          	f->eax = open ((char *) *pop(esp));
-          	break;
-        case SYS_FILESIZE:
-	  		f->eax = filesize (*pop(esp));
-	  		break;
-        case SYS_READ:
-          	f->eax = read (*pop(esp), (void *) *pop(esp), *pop(esp));
-          	break;
-        case SYS_WRITE:
-          	f->eax = write (*pop(esp), (void *) *pop(esp), *pop(esp));
-          	break;
-        case SYS_SEEK:
-          	seek (*pop(esp), *pop(esp));
-          	break;
-        case SYS_TELL:
-          	f->eax = tell (*pop(esp));
-          	break;
-        case SYS_CLOSE:
-          	close (*pop(esp));
-          	break;
-        default:
-          	break;
-      	}	
-	}
+  printf ("system call!\n");
+
+  //peek at system call args from stack -> call corresponding handler
+  int *esp = f->esp;
+  int syscall_number = *esp;
+  if(!is_user_vaddr(syscall_number)){
+    exit(-1);
+  }
+
+  switch (syscall_number){
+      case SYS_HALT:
+          halt();
+          break;
+      case SYS_EXIT:
+          f->error_code = *(esp + 4); //return to kernel?
+          exit(*(esp + 4));
+          break;
+      case SYS_EXEC:
+    	    f->eax = exec(*(esp + 4));
+    	    break;
+      case SYS_WAIT:
+          wait(*(esp + 4));
+          break;
+      case SYS_CREATE:
+          f->eax = create(*(esp + 4), *(esp + 5));
+          break;
+      case SYS_REMOVE:
+          f->eax = remove(*(esp + 4));
+          break;
+      case SYS_OPEN:
+          f->eax = open(*(esp + 4));
+          break;
+      default:
+          break;
+  }
+
+  thread_exit ();
 }
